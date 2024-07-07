d67cdaa75af7bc199c97e294f9bede2f61cbbdfb
deidmon <paul.dedios@alum.udep.edu.pe>
2024-07-06 22:01:50 -0500

    Modulo 3

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
+while (numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-console.log(numeroUsuario);
+    console.log(numeroUsuario);
 
-if (numeroUsuario == numeroSecreto) {
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else {
-    alert('Lo siento, no acertaste el número');
+    if (numeroUsuario == numeroSecreto) {
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}.`);
+    } else {
+        if (numeroUsuario > numeroSecreto){
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        //alert('Lo siento, no acertaste el número');
+        intentos += 1;
+        palabraVeces = 'veces'
+    }
 }
+    
