675a014e0e36230228fb31d2383a4b6836f3ff00
deidmon <paul.dedios@alum.udep.edu.pe>
2024-07-06 23:53:34 -0500

    Modulo 5

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-let numeroSecreto = Math.floor(Math.random()*10)+1;
+let numeroMaximoPosible = 10;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
 let numeroUsuario = 0;
 let intentos = 1;
 //let palabraVeces = 'vez';
 let maximosIntentos = 3;
 
 while (numeroUsuario != numeroSecreto){
-    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
+    numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible} por favor:`));
 
     //console.log(numeroUsuario);
 
