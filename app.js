0ac83c92dd00ce062d0695f9a8c1b5924bdb1275
deidmon <paul.dedios@alum.udep.edu.pe>
2024-07-06 23:48:25 -0500

    Modulo 4

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-let numeroSecreto = 6;
+let numeroSecreto = Math.floor(Math.random()*10)+1;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
-while (numeroUsuario != numeroSecreto){
-    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+//let palabraVeces = 'vez';
+let maximosIntentos = 3;
 
-    console.log(numeroUsuario);
+while (numeroUsuario != numeroSecreto){
+    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
+
+    //console.log(numeroUsuario);
 
     if (numeroUsuario == numeroSecreto) {
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}.`);
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}.`);
     } else {
         if (numeroUsuario > numeroSecreto){
             alert('El número secreto es menor');
@@ -17,7 +19,11 @@ while (numeroUsuario != numeroSecreto){
         }
         //alert('Lo siento, no acertaste el número');
         intentos += 1;
-        palabraVeces = 'veces'
+        //palabraVeces = 'veces'
+        if (intentos >maximosIntentos){
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos.`);
+            break;
+        }
     }
 }
     
